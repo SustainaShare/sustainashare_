eab95fa01dbef09a0140c188563caadb11790381
Kevin Isom <47275702+Isomkevin@users.noreply.github.com>
2024-05-30 16:39:51 +0300

    Changes to DonationsPage and Donation Requests Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import { SignInPage,RegisterPage } from './pages/AuthPage';
-import DonationsPage from './pages/DonationsPage';
+import RequestsPage from './pages/DonationsRequestsPage';
+// import DonationsPage from './pages/DonationsPage';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<SignInPage />} />
         <Route path="/register" element={<RegisterPage />} /> 
-        <Route path="/donation-dashboard" element={<DonationsPage />} />
+        <Route path="/donation-requests" element={<RequestsPage />} />
+        {/* <Route path="/donation-list" element={<DonationsPage />} /> */}
         {/* Add other routes here */}
       </Routes>
     </Router>
