bd53e05a63b1ef20ea8b2c82510209d02dde8aac
Kevin Isom <47275702+Isomkevin@users.noreply.github.com>
2024-05-30 14:23:29 +0300

    SignIn Page Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import SignInPage from './pages/SignInPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/login" element={<HomePage />} />
         {/* Add other routes here */}
       </Routes>
     </Router>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-        <p className="text-center text-gray-400 text-sm">© 2024 Your Company. All rights reserved.</p>
+        <p className="text-center text-gray-400 text-sm">© 2024 SustainAShare. All rights reserved.</p>
       </div>
     </footer>
   );
