3dc6e77ae61c017cfc26aff502826766ab4bca68
Kevin Isom <47275702+Isomkevin@users.noreply.github.com>
2024-05-30 17:16:01 +0300

    Chages in Logo and images URLs

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -87,7 +87,7 @@ export function RegisterPage() {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            src={logo}
             alt="SustainAShare Logo"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
